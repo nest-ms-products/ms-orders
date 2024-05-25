e3c64489fc05617b5e7c50aef476422a4c500e0f
Jean Osco <willjean29@gmail.com>
2024-05-25 12:39:06 -0500

    feat: setup envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+  logger.log(`Ms-Ordes running on port ${envs.port}`);
 }
 bootstrap();
